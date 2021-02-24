0c58924319423c493c4c54c872e5c860b6152e8c
junyoung <jy940608@naver.com>
2021-02-25 00:12:32 +0900

    댓글 수정하기

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,11 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostList.as_view(), name='list'),
     path('<int:pk>/', views.PostDetail.as_view(), name='detail'),
+    path('<int:pk>/new_comment/', views.new_comment, name='comment-create'),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view(), name='comment-update'),
     path('category/<str:slug>/', views.category_page, name='category'),
     path('tag/<str:slug>/', views.tag_page, name='tag'),
     path('create_post/', views.PostCreate.as_view(), name='create'),
     path('update_post/<int:pk>/', views.PostUpdate.as_view(), name='update')
+
 ]
