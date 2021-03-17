90a281923404b0893768d812ff9f50c74f16eb11
junyoung <jy940608@naver.com>
2021-03-17 18:27:05 +0900

    도커에 gunicorn 적용

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
